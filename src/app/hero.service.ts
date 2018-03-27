5d847661deccf3531f80e81f1cebd1f62a88aeeb
Sara Kevorkian <sdkevorkian@gmail.com>
2018-03-27 12:32:12 -0700

    messages service and component added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,12 +3,17 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageSVC: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
+    // to do - send message after fetching
+    this.messageSVC.add('HeroService: fetched heroes');
     return of(HEROES);
   }
+
 }
