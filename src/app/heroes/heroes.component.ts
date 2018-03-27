7f0e3ce506bdecef126b1b19065a3492536d5250
Sara Kevorkian <sdkevorkian@gmail.com>
2018-03-27 12:11:12 -0700

    Hero service using observables

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +10,17 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = HEROES;
-  constructor() { }
+  heroes: Hero[];
+  constructor(private heroSVC: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroSVC.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
