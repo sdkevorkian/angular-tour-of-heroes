ace44c35ddcc0679d98685d81f41ab65c7ae0c2c
Sara Kevorkian <sdkevorkian@gmail.com>
2018-03-28 11:26:18 -0700

    added search functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -61,4 +61,13 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching ${term}`)),
+      catchError(this.handleError<Hero[]>('SearchHeroes', []))
+    );
+  }
 }
